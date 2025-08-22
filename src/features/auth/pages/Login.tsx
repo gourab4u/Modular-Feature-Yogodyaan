cec4d41ebfa6feebff2058e9a8833f17a2cb0ce4
gourab4u <gourab.master@gmail.com>
2025-08-22 22:41:54 +0530

    Removed Facebook

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -63,7 +63,7 @@ export function Login() {
     return Object.keys(newErrors).length === 0
   }
 
-  const handleSocialSignIn = async (provider: 'google' | 'facebook') => {
+  const handleSocialSignIn = async (provider: 'google') => {
     setSocialLoading(provider)
     setErrors({})
 
@@ -209,23 +209,7 @@ export function Login() {
               )}
             </Button>
 
-            <Button
-              onClick={() => handleSocialSignIn('facebook')}
-              loading={socialLoading === 'facebook'}
-              variant="outline"
-              className="w-full flex items-center justify-center space-x-3 py-3 border-gray-300 dark:border-gray-600 hover:border-gray-400 dark:hover:border-gray-500 hover:bg-gray-50 dark:hover:bg-gray-800"
-            >
-              {socialLoading === 'facebook' ? (
-                <span>Connecting...</span>
-              ) : (
-                <>
-                  <svg className="w-5 h-5" viewBox="0 0 24 24" fill="#1877F2">
-                    <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-                  </svg>
-                  <span>Continue with Facebook</span>
-                </>
-              )}
-            </Button>
+            {/* Facebook sign-in removed */}
           </div>
 
           {/* Divider */}
